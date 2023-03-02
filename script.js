8dcb607ebc67f9ee9ad3c3c9aaae97d51e22ccab
Sycinz <gronbosof@gmail.com>
2023-03-02 15:48:08 +0100

    Creating array and few more changes

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-var map = new Array[10][10]
+const map = [
+    [0,1,2,3,4,5,6,7,8,9],
+    [0,1,2,3,4,5,6,7,8,9]
+]
 
-function snakePosRefresh() {
+var canvas = document.querySelector('canvas')
 
+let snake = {
+    size: 80, // in pixels for now
+    color: "green"
 }
+
+function snakePosRefresh() {}
 
 function canvasDrawing(x, y) {
-    const canvas = document.querySelector('#game')
-    let ctx = canvas.getContext("2d")
+    
+    var ctx = canvas.getContext("2d")
 
-    ctx.moveTo(map[x][y])
+    ctx.fillStyle = snake.color
+    ctx.fillRect(map[0][x] * snake.size, map[1][y] * snake.size, snake.size, snake.size)
+
+    snake.posX = x
+    snake.posY = y
+
+    // return snake.posX, snake.posY
 }
 
-function snakeMove() {
-    canvasDrawing(0, 0);
+function changeDirection() {
+    document.addEventListener('keyup', (event) => {
+        document.innerHTML += (event.key)
+    })
 }
 
-function gameLoop() {
-
+window.onload = function gameLoop() {
+    // for(let i; i <= 10; i++) {
+    //     canvasDrawing(Math.floor(Math.random() * 9), Math.floor(Math.random() * 9))
+    // }
+   canvasDrawing(Math.floor(Math.random() * map[0].length), Math.floor(Math.random() * map[0].length))
 }
